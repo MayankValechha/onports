780005e1fe70279fc0ca7205e339f1958d36cef1
MayankValechha <valechhamayank12@gmail.com>
2018-09-16 20:33:17 +0530

    Added Parse connectivity for Register Page.

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 //DEFINING FORM ELEMENTS
 const registerForm = document.querySelector('form');
-registerForm.addEventListener('submit', runEvent);
+registerForm.addEventListener('submit', registerUser);
 
-const firstName = document.getElementById('fName');
-const lastName = document.getElementById('lName');
-const mobileNum = document.getElementById('moNum');
-const pass = document.getElementById('pass');
-const confirmPass = document.getElementById('conPass');
-let all
+let firstName = document.getElementById('fName');
+let lastName = document.getElementById('lName');
+let mobileNum = document.getElementById('moNum');
+let pass = document.getElementById('pass');
+let confirmPass = document.getElementById('conPass');
+let email = document.getElementById('email');
+
+//LOGIN ELEMENTS
+const loginForm = document.querySelector('form');
+loginForm.addEventListener('submit', loginUser);
+
+let userName = document.getElementById('username');
+let password = document.getElementById('username');
+
 
 //ADDING EVENT LISTENERS
 firstName.addEventListener('blur', checkFName);
@@ -41,7 +49,7 @@ function checkLName(){
 
 function checkMobileNo(){
 	//For Mobile Number check.
-	if(mobileNum.value == '' || mobileNum.value.length !=10 || isNaN(mobileNum.input)){
+	if(mobileNum.value == '' || mobileNum.value.length !=10){
 		mobileNum.style.border = '1px solid red';
 	}
 	else{
@@ -68,7 +76,32 @@ function checkConfirmPass(){
 	}
 }
 
+Parse.initialize("PgKKTeokkD5m9RwFUsdumjY4nik1KnYDtkW5LwGI", "rdCO861N9xGCA7N2waGl1yzShT8XvHTRGHimBfYz"); 
+Parse.serverURL = "https://parseapi.back4app.com/";
 
-function runEvent() {
-//	Will Work on it Later.
+function registerUser() {
+	
+	var newUser = new Parse.User();
+	newUser.set("username", mobileNum.value);
+	newUser.set("firstName", firstName.value);
+	newUser.set("lastName", lastName.value);
+	newUser.set("contactNumber", Number(mobileNum.value));
+	newUser.set("password", pass.value);
+	newUser.set("email", email.value);
+
+	newUser.save()
+	.then((newUser) => {
+  		alert('Your Account has been created!, Welcome to Onports.');
+	}, 
+	(error) => {
+  	
+  		alert('Failed to create new object, with error code: ' + error.message);
+	});
+	
+}
+
+function loginUser(){
+	
+	const newUser = Parse.User.logIn(userName.value, password.value);
+	console.log(user);
 }
