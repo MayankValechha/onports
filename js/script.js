8a16d8b041e7192d8c1ab1cc6dab0590103f8c86
MayankValechha <valechhamayank12@gmail.com>
2018-09-20 21:13:54 +0530

    Cleaning up unwanted files.

--- js/script.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('load',init);
-
-let username = document.querySelector('#username');
-
-function init(){
-    if(username.value = ''){
-        console.log("Fill out values");
-    }
-}
